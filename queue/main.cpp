03d96ca459b8033128e0b803e0bb8bf4d27338fa
keinahas <kaidrielle@gmail.com>
2020-08-21 15:20:08 +0900

    error handling

--- queue/main.cpp
+++ queue/main.cpp
@@ -4,20 +4,27 @@ int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	Queue<int>* q = new Queue<int>();
-	cout << "newest:" << q->frontEle() << endl;
-	cout << "oldest:" << q->backEle() << endl;
-	cout << "size: " << q->returnSize() << endl;
-	q->popEle();
-	q->PushEle(1);
-	q->PushEle(2);
-	q->PushEle(3);
-	cout << "newest:" << q->frontEle() << endl;
-	cout << "oldest:" << q->backEle() << endl;
-	cout << "size: " << q->returnSize() << endl;
-	q->printQueue();
-	q->popEle();
-	q->printQueue();
-	cout << "newest:" << q->frontEle() << endl;
-	cout << "oldest:" << q->backEle() << endl;
-	cout << "size: " << q->returnSize() << endl;
+	try
+	{
+		cout << "newest:" << q->frontEle() << endl;
+		cout << "oldest:" << q->backEle() << endl;
+		cout << "size: " << q->returnSize() << endl;
+		q->popEle();
+		q->PushEle(1);
+		q->PushEle(2);
+		q->PushEle(3);
+		cout << "newest:" << q->frontEle() << endl;
+		cout << "oldest:" << q->backEle() << endl;
+		cout << "size: " << q->returnSize() << endl;
+		q->printQueue();
+		q->popEle();
+		q->printQueue();
+		cout << "newest:" << q->frontEle() << endl;
+		cout << "oldest:" << q->backEle() << endl;
+		cout << "size: " << q->returnSize() << endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
